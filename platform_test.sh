9e09c671e3496f82e193f3310c63bc741f749fbb
Jose Manuel Sánchez González <jose.m.sanchez.glez@gmail.com>
2021-05-28 05:06:18 +0200

    Complete README.md & strengthen with a new check

--- platform_test.sh
+++ platform_test.sh
@@ -2,11 +2,21 @@
 
 ORIGIN_BUCKET=$1
 DESTINY_BUCKET=$2
+MOVE_ERROR_CHECK=1
+BUCKETS_ERROR_CHECK=0
 
-ERROR_CHECK=1
-while [ $ERROR_CHECK != 0 ]
+while [[ $MOVE_ERROR_CHECK != 0 ]]
 do
-	aws s3 mv s3://$1 s3://$2 --recursive
-	ERROR_CHECK=$(echo $?)
+	aws s3api head-bucket --bucket $ORIGIN_BUCKET
+	BUCKETS_ERROR_CHECK=$?
+	aws s3api head-bucket --bucket $DESTINY_BUCKET
+	BUCKETS_ERROR_CHECK=$( expr $BUCKETS_ERROR_CHECK + $? )
+
+	if [[ $BUCKETS_ERROR_CHECK != 0 ]]; then
+		break
+	fi
+
+	aws s3 mv s3://$ORIGIN_BUCKET s3://$DESTINY_BUCKET --recursive
+	MOVE_ERROR_CHECK=$?
 done
 
